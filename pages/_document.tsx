2cff46c12be3ed31621d550827e106fc00b9df0a
Stathis Moraitidis <emoraitidis@gmail.com>
2023-01-05 11:27:10 +0200

    Update _document with netlify

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,23 +3,28 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              if (window.netlifyIdentity) {
-                window.netlifyIdentity.on("init", user => {
-                  if (!user) {
-                    window.netlifyIdentity.on("login", () => {
-                      document.location.href = "/admin/";
-                    });
-                  }
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+              if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                  document.location.href = "/admin/";
                 });
               }
-          `,
+            });
+          }
+      `,
           }}
         />
       </body>
