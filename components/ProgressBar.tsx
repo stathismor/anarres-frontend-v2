c5b64d5c589e8da76dd1c71cc7342fe1c36d02bc
Stathis Moraitidis <emoraitidis@gmail.com>
2023-01-21 23:13:42 +0000

    Add progress bar transition

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -13,7 +13,11 @@ export function ProgressBar({ elapsed, duration, isPlaying }: Props) {
     <>
       <div className="flex bg-gray-100 h-0.5 sm:h-1">
         <div
-          className={isPlaying ? "bg-red-500" : "bg-gray-400"}
+          className={
+            isPlaying
+              ? "transition-property: background-color; bg-red-500 duration-300 ..."
+              : "transition-property: background-color; bg-gray-400 duration-300 ..."
+          }
           style={{ width: `${getProgressPercentage(elapsed, duration)}%` }}
         />
       </div>
