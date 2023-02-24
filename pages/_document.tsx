663263692f790a8120d8e9388a58c8718932b14f
Theo Mironidis <mironidis.gr@gmail.com>
2023-02-24 17:24:14 +0000

    feat: update site meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,22 @@ export default function Document() {
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png" />
-        <meta name="theme-color" content="#000" />
+        <meta name="application-name" content="Anarres.fm" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Anarres.fm" />
+        <meta name="description" content="Anarres.fm | Music with no boundaries" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#3350A1" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
+
+
       </Head>
       <body>
         <Main />
