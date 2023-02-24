358f3051199bd71acbdfc983484a0589d575c1b3
Theo Mironidis <mironidis.gr@gmail.com>
2023-02-24 17:25:29 +0000

    feat: lowercase manifest name

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,8 +25,6 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-
-
       </Head>
       <body>
         <Main />
