c309d2acc083512c0d699cb98c9ad4e2cf1deeb7
Theo Mironidis <mironidis.gr@gmail.com>
2023-02-26 11:01:04 +0000

    feat: add icon and instructions

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,6 @@ export default function Document() {
         />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="application-name" content="Anarres.fm" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -21,10 +20,9 @@ export default function Document() {
         <meta name="msapplication-TileColor" content="#3350A1" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/images/icons/ios/152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/ios/180.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/images/icons/ios/167.png" />
       </Head>
       <body>
         <Main />
